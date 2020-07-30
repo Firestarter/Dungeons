7f7ec710422e3adcafd059ec37b634d71b255809
Jeroen Verwimp <jeroen@verwimp.me>
2020-07-30 17:01:00 +0200

    Project structure

---
+++ lobby/src/main/java/com/firestartermc/dungeons/DungeonsLobby.java
@@ -0,0 +1,17 @@
+package com.firestartermc.dungeons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DungeonsLobby extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+}
